33673168802bcef6be4b39c39c0aa8254ec6666e
Cao Mai <caomai@rocketmail.com>
2021-04-12 21:41:17 -0700

    Upload first 2 projects to Github

---
+++ FlagsQuiz/FlagsQuiz/FlagsQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsQuizApp.swift
+//  FlagsQuiz
+//
+//  Created by Cao Mai on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagsQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cao Mai on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
