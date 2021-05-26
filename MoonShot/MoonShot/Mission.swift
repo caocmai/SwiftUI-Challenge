81c0a71eb4beede476cad3b2f8b41633bf00c606
Cao Mai <caomai@rocketmail.com>
2021-05-25 21:49:17 -0700

    Add generics to decode function

---
+++ MoonShot/MoonShot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  MoonShot
+//
+//  Created by Cao Mai on 5/25/21.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
