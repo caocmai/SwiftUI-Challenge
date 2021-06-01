31f44f3da3ed5ebc674be82617f911c859ffde88
Cao Mai <caomai@rocketmail.com>
2021-05-31 21:40:23 -0700

    Start CupcakeCorner project

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Cao Mai on 5/31/21.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    @Published var name = "Cao Mai"
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Cao Mai on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
