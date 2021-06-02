498c84147b546cb183e7bb3f6ec111d749d49d3c
Cao Mai <caomai@rocketmail.com>
2021-06-01 21:33:49 -0700

    Add TextFields for user address

--- CupcakeCorner/CupcakeCorner/View/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/View/CheckoutView.swift
@@ -11,7 +11,7 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Checkout View")
     }
 }
 
