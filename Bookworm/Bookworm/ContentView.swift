3109ff49a05390fb95de59c4dade033f03f30110
Cao Mai <caomai@rocketmail.com>
2021-06-02 20:47:34 -0700

    Add button to generate random names

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
 
     
@@ -19,6 +19,20 @@ struct ContentView: View {
                         Text(student.name ?? "Unknown")
                     }
                 }
+                
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    let student = Student(context: self.moc)
+                    student.id = UUID()
+                    student.name = "\(chosenFirstName) \(chosenLastName)"
+                    try? self.moc.save()
+
+                }
+                
             }
         }
 }
