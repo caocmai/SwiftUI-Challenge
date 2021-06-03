10334ba037245b2da678fdcb83a0ca8636c5bb20
Cao Mai <caomai@rocketmail.com>
2021-06-02 20:22:11 -0700

    Add Bookworm project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Cao Mai on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
