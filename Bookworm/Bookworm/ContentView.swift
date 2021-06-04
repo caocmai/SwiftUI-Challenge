3802b4c3947409652e531c63ef37121b72f7f3a9
Cao Mai <caomai@rocketmail.com>
2021-06-03 21:29:53 -0700

    Add Book model to Core Data

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -9,31 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
 
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
     
     var body: some View {
-            VStack {
-                List {
-                    ForEach(students, id: \.id) { student in
-                        Text(student.name ?? "Unknown")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
                     }
                 }
-                
-                Button("Add") {
-                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
 
-                    let chosenFirstName = firstNames.randomElement()!
-                    let chosenLastName = lastNames.randomElement()!
-                    let student = Student(context: self.moc)
-                    student.id = UUID()
-                    student.name = "\(chosenFirstName) \(chosenLastName)"
-                    try? self.moc.save()
+                Section {
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text("\($0)")
+                        }
+                    }
 
+                    TextField("Write a review", text: $review)
                 }
-                
+
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
+
+                        try? self.moc.save()
+                    }
+                }
             }
+            .navigationBarTitle("Add Book")
+        }
         }
 }
 
