cb0c420a3a91238c36841088a0d462bc2b29784d
Cao Mai <caomai@rocketmail.com>
2021-06-03 21:37:07 -0700

    Book saves to Core Data

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,55 +10,24 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
-    
-    @State private var title = ""
-    @State private var author = ""
-    @State private var rating = 3
-    @State private var genre = ""
-    @State private var review = ""
 
-    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    @State private var showingAddScreen = false
     
     var body: some View {
         NavigationView {
-            Form {
-                Section {
-                    TextField("Name of book", text: $title)
-                    TextField("Author's name", text: $author)
-
-                    Picker("Genre", selection: $genre) {
-                        ForEach(genres, id: \.self) {
-                            Text($0)
-                        }
-                    }
-                }
-
-                Section {
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text("\($0)")
-                        }
-                    }
-
-                    TextField("Write a review", text: $review)
-                }
-
-                Section {
-                    Button("Save") {
-                        let newBook = Book(context: self.moc)
-                        newBook.title = self.title
-                        newBook.author = self.author
-                        newBook.rating = Int16(self.rating)
-                        newBook.genre = self.genre
-                        newBook.review = self.review
-
-                        try? self.moc.save()
-                    }
-                }
-            }
-            .navigationBarTitle("Add Book")
+           Text("Count: \(books.count)")
+               .navigationBarTitle("Bookworm")
+               .navigationBarItems(trailing: Button(action: {
+                   self.showingAddScreen.toggle()
+               }) {
+                   Image(systemName: "plus")
+               })
+               .sheet(isPresented: $showingAddScreen) {
+                   AddBookView().environment(\.managedObjectContext, self.moc)
+               }
         }
-        }
+    }
+       
 }
 
 struct ContentView_Previews: PreviewProvider {
