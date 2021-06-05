c6a05806f5a52853e0c4c9e0823a60ce9304f2b6
Cao Mai <caomai@rocketmail.com>
2021-06-04 22:05:45 -0700

    Programmatically pop view after delete

--- Bookworm/Bookworm/BookDetailView.swift
+++ Bookworm/Bookworm/BookDetailView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 import CoreData
 
 struct BookDetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
     var book: Book
     var body: some View {
         GeometryReader { geometry in
@@ -16,7 +20,7 @@ struct BookDetailView: View {
                 ZStack(alignment: .bottomTrailing) {
                     Image(self.book.genre ?? "Fantasy")
                         .frame(maxWidth: geometry.size.width)
-
+                    
                     Text(self.book.genre?.uppercased() ?? "FANTASY")
                         .font(.caption)
                         .fontWeight(.black)
@@ -31,34 +35,51 @@ struct BookDetailView: View {
                 Text(self.book.author ?? "Unknown author")
                     .font(.title)
                     .foregroundColor(.secondary)
-
+                
                 Text(self.book.review ?? "No review")
                     .padding()
-
+                
                 RatingView(rating: .constant(Int(self.book.rating)))
                     .font(.largeTitle)
-
+                
                 Spacer()
             }
         }
         .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
-
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                self.deleteBook()
+            }, secondaryButton: .cancel()
+            )
+        }
+        .navigationBarItems(trailing: Button(action: {
+            self.showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        try? self.moc.save()
+        // Goes back to the previous screen
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
 struct BookDetailView_Previews: PreviewProvider {
     static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
-
-        static var previews: some View {
-            let book = Book(context: moc)
-            book.title = "Test book"
-            book.author = "Test author"
-            book.genre = "Fantasy"
-            book.rating = 4
-            book.review = "This was a great book; I really enjoyed it."
-
-            return NavigationView {
-                BookDetailView(book: book)
-            }
+    
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test book"
+        book.author = "Test author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book; I really enjoyed it."
+        
+        return NavigationView {
+            BookDetailView(book: book)
         }
+    }
 }
