858f183aab4ad8c3bcdb95de8dcf6aa27f7a1e06
Cao Mai <caomai@rocketmail.com>
2021-06-06 22:07:21 -0700

    Filtered List View is now a generic view

--- CoreData1/CoreData1/FilteredListView.swift
+++ CoreData1/CoreData1/FilteredListView.swift
@@ -6,15 +6,24 @@
 //
 
 import SwiftUI
+import CoreData
 
-struct FilteredListView: View {
+struct FilteredListView<T: NSManagedObject, Content: View>: View {
+    var fetchRequest: FetchRequest<T>
+    var singers: FetchedResults<T> { fetchRequest.wrappedValue }
+
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(fetchRequest.wrappedValue, id: \.self) { singer in
+            self.content(singer)
+        }
+    }
+
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        fetchRequest = FetchRequest<T>(entity: T.entity(), sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
     }
 }
 
-struct FilteredListView_Previews: PreviewProvider {
-    static var previews: some View {
-        FilteredListView()
-    }
-}
