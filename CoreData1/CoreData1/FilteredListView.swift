6eacf7ecd0e60a00fa47df29b9aab95e395e2a60
Cao Mai <caomai@rocketmail.com>
2021-06-06 21:52:19 -0700

    Add Singer Enity in Core Data

---
+++ CoreData1/CoreData1/FilteredListView.swift
@@ -0,0 +1,20 @@
+//
+//  FilteredListView.swift
+//  CoreData1
+//
+//  Created by Cao Mai on 6/6/21.
+//
+
+import SwiftUI
+
+struct FilteredListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilteredListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilteredListView()
+    }
+}
---
+++ CoreData1/CoreData1/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreData1
+//
+//  Created by Cao Mai on 6/6/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
