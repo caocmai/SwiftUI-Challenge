af3171e94327c71f270b26906e569212eccb3267
Cao Mai <caomai@rocketmail.com>
2021-06-07 21:31:05 -0700

    Fetch one to many objects

---
+++ CoreDataRelationships/CoreDataRelationships/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataRelationships
+//
+//  Created by Cao Mai on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreDataRelationships/CoreDataRelationships/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataRelationships
+//
+//  Created by Cao Mai on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
