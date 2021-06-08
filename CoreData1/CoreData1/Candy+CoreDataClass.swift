8964b5ffefc7ff903699633c240d324bb876f91b
Cao Mai <caomai@rocketmail.com>
2021-06-07 21:08:08 -0700

    Update Core Data models to Candy and Country

---
+++ CoreData1/CoreData1/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreData1
+//
+//  Created by Cao Mai on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ CoreData1/CoreData1/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreData1
+//
+//  Created by Cao Mai on 6/7/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
