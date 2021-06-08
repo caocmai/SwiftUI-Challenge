d7a6dba4f800ef79753d8519208515d3ac2895bc
Cao Mai <caomai@rocketmail.com>
2021-06-07 21:22:58 -0700

    Add Core Data Relationships project

---
+++ CoreDataRelationships/CoreDataRelationships/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataRelationships
+//
+//  Created by Cao Mai on 6/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
